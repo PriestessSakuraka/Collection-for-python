c1a793f6730f5b9e1f4f154eb1671d5953624d5c
kamu <48278137+PriestessSakuraka@users.noreply.github.com>
2022-11-06 17:24:33 +0900

    Create Collection.py

---
+++ Collection.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+
+class Collection(OrderedDict):
+
+    def __init__(self, dict = {}):
+        super().__init__(dict)
+
+    def set(self, key, value):
+        self[key] = value
+
+    def find(self, fn = None):
+        args_len = len(fn.__code__.co_varnames[:fn.__code__.co_argcount])
+
+        for k, v in self.items():
+            if args_len == 1:
+                if fn(v): return v
+            if args_len == 2:
+                if fn(v, k): return v
+            if args_len == 3:
+                if fn(v, k, self): return v
